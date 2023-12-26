41e613cdf9e9516c217642425a851a26ebd01bb1
unknown <luizhemrique.fnandes@gmail.com>
2023-12-26 13:40:19 -0300

    Criando estrutura do projeto

---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+// src/components/Info.js
+import React from 'react';
+
+const Info = () => {
+    return (
+        <div>
+            <h2>Informações Pessoais</h2>
+        </div>
+    );
+};
+
+export default Info;
